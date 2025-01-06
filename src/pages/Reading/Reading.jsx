7a7ebfd61e6c58f1f2fb28456258d0423aee92d5
Jackie <actions@github.com>
2025-01-06 10:07:18 -0500

    Fixed

--- src/pages/Reading/Reading.jsx
+++ src/pages/Reading/Reading.jsx
@@ -136,8 +136,8 @@ const Reading = ({ handleSwitchToRegister, handleSwitchToLogin }) => {
 
     
         const prompt = `My question is: ${question}. I drew ${cards.length} cards. They are: ${cardStates}. Return 35 words per card, formatted like 'Card Name (Reversal Status) + ':'', separated by line break, start with the card name at the beginning. Take upright/reverse into account. Lastly, give one paragraph summary start with 'Summary:'. (35 words).`;
-        
-        const response = await axios.post('https://journal-app-backend-8szt.onrender.com/sendMsgToOpenAI', {
+
+        const response = await axios.post('http://localhost:5000/sendMsgToOpenAI', {
             userMessage: prompt,
         }, {
             timeout: 60000, 
